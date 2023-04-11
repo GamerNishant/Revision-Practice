447c1b5db6d1c4aa87f85c8c131428343697b22b
Nishant Singh <official007nishant@gmail.com>
2023-04-11 15:24:03 +0530

    Staircase search

--- 2d-mat-search.cpp
+++ 2d-mat-search.cpp
@@ -1,24 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+// staircase binary search
+
+int main()
+{
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int n, m;
-        cin>>n>>m;
+        cin >> n >> m;
         int mat[n][m];
 
-        for(int i = 0; i<n; i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = 0;j<m; j++)
+            for (int j = 0; j < m; j++)
             {
-                
+                cin >> mat[i][j];
             }
         }
-    }
-    return 0;
 
+        int target;
+        cin >> target;
+
+        int i = 0;
+        int j = m - 1;
+        bool flag = false;
+        while (i < n and j >= 0)
+        {
+            if (mat[i][j] == target)
+            {
+                flag = true;
+                break;
+            }
+            else if (mat[i][j] < target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
 }
